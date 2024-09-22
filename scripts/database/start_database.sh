477197a9d690070cb66c98cd8a30e88d1f0ca08f
Riley Smith <Rileys1000@gmail.com>
2024-09-22 20:17:59 +1200

    added more info to the config.yaml file

--- scripts/database/start_database.sh
+++ scripts/database/start_database.sh
@@ -22,23 +22,39 @@ if check_image_exists $IMAGE_NAME; then
     echo "Docker image $IMAGE_NAME found. Running the container..."
 
     echo "Debug: IMAGE_NAME=$IMAGE_NAME"
+    echo "Debug: DOCKER_NAME=$DOCKER_NAME"
     echo "Debug: DB_NAME=$DB_NAME"
     echo "Debug: DB_PORT=$DB_PORT"
-    echo "Debug: DB_ADMIN_PASSWORD=$DB_ADMIN_PASSWORD"
+    echo "Debug: DB_USERNAME=$DB_USERNAME"
+    echo "Debug: DB_PASSWORD=$DB_PASSWORD"
     echo "Debug: DB_HOST_MOUNT_POINT=$DB_HOST_MOUNT_POINT"
 
-    # Run the docker container
-    docker run --name $DB_NAME \
-        -p $DB_PORT:1521 \
-        --ulimit nofile=1024:65536 \
-        --ulimit nproc=2047:16384 \
-        --ulimit stack=10485760:33554432 \
-        --ulimit memlock=3221225472 \
-        -e ORACLE_PWD=$DB_ADMIN_PASSWORD \
-        -e ENABLE_ARCHIVELOG=true \
-        -e ENABLE_FORCE_LOGGING=true \
-        -v $DB_HOST_MOUNT_POINT:/opt/oracle/oradata \
-        $IMAGE_NAME
+    # Check if container exists
+    container_status=$(docker ps -a --filter "name=$DOCKER_NAME" --format "{{.Status}}")
+
+    if [[ $container_status == *"Up"* ]]; then
+        echo "The container $DOCKER_NAME is already running."
+    elif [[ $container_status == *"Exited"* ]]; then
+        echo "The container $DOCKER_NAME exists but is stopped. Starting it..."
+        docker start $DOCKER_NAME
+    else
+        echo "Container $DOCKER_NAME does not exist. Creating and running the container..."
+        docker run --name $DOCKER_NAME \
+            -p $DB_PORT:1521 \
+            --ulimit nofile=1024:65536 \
+            --ulimit nproc=2047:16384 \
+            --ulimit stack=10485760:33554432 \
+            --ulimit memlock=3221225472 \
+            -e ORACLE_PWD=$DB_ADMIN_PASSWORD \
+            -e ENABLE_ARCHIVELOG=true \
+            -e ENABLE_FORCE_LOGGING=true \
+            -e ORACLE_SID=$DB_SID \
+            -e ORACLE_USER=$DB_USERNAME \
+            -e ORACLE_PWD=$DB_PASSWORD \
+            -v $DB_HOST_MOUNT_POINT:/opt/oracle/oradata \
+            $IMAGE_NAME
+    fi
+
 
 else
     #
