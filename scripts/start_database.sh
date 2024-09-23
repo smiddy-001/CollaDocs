7f24efd355af1d91344ef1368c21f218245a9c5a
Riley Smith <Rileys1000@gmail.com>
2024-09-23 23:25:28 +1200

    cleaned up the config so it doesnt have values oracle doesnt support, like a username... for some reason, anyways, I run a command to create a database user, and rename the SYS user, but I still had to change the config to match the two distinct users, that we may need to store

--- scripts/start_database.sh
+++ scripts/start_database.sh
@@ -1,8 +1,9 @@
 MY_SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-CONFIG_FOLDER=$MY_SCRIPT_DIR/../../config/
+WORKSPACE_FOLDER=$MY_SCRIPT_DIR/..
+CONFIG_FOLDER=$WORKSPACE_FOLDER/config
+DOCS=$WORKSPACE_FOLDER/docs
 CONFIG_YAML_FILE=$CONFIG_FOLDER/config.yaml
 CONFIG_SCRIPT_FILE=$CONFIG_FOLDER/load_config.sh
-
 
 source $CONFIG_SCRIPT_FILE --config $CONFIG_YAML_FILE --mode test
 # source $CONFIG_SCRIPT_FILE --config $CONFIG_YAML_FILE --mode test
@@ -14,6 +15,15 @@ check_image_exists() {
   return $?
 }
 
+{
+  docker ps -q
+} || {
+  echo
+  echo "Docker is not running. Please start docker on your computer"
+  echo "When docker has finished starting up press [ENTER] to continue"
+  echo
+  read
+}
 
 if check_image_exists $IMAGE_NAME; then
     #
@@ -22,7 +32,7 @@ if check_image_exists $IMAGE_NAME; then
     echo "Docker image $IMAGE_NAME found. Running the container..."
 
     echo "Debug: IMAGE_NAME=$IMAGE_NAME"
-    echo "Debug: DOCKER_NAME=$DOCKER_NAME"
+    echo "Debug: DOCKER_NAME=$CONTAINER_NAME"
     echo "Debug: DB_NAME=$DB_NAME"
     echo "Debug: DB_PORT=$DB_PORT"
     echo "Debug: DB_USERNAME=$DB_USERNAME"
@@ -30,16 +40,16 @@ if check_image_exists $IMAGE_NAME; then
     echo "Debug: DB_HOST_MOUNT_POINT=$DB_HOST_MOUNT_POINT"
 
     # Check if container exists
-    container_status=$(docker ps -a --filter "name=$DOCKER_NAME" --format "{{.Status}}")
+    container_status=$(docker ps -a --filter "name=$CONTAINER_NAME" --format "{{.Status}}")
 
     if [[ $container_status == *"Up"* ]]; then
-        echo "The container $DOCKER_NAME is already running."
+        echo "The container $CONTAINER_NAME is already running."
     elif [[ $container_status == *"Exited"* ]]; then
-        echo "The container $DOCKER_NAME exists but is stopped. Starting it..."
-        docker start $DOCKER_NAME
+        echo "The container $CONTAINER_NAME exists but is stopped. Starting it..."
+        docker start $CONTAINER_NAME
     else
-        echo "Container $DOCKER_NAME does not exist. Creating and running the container..."
-        docker run --name $DOCKER_NAME \
+        echo "Container $CONTAINER_NAME does not exist. Creating and running the container..."
+        docker run --name $CONTAINER_NAME \
             -p $DB_PORT:1521 \
             --ulimit nofile=1024:65536 \
             --ulimit nproc=2047:16384 \
@@ -49,12 +59,21 @@ if check_image_exists $IMAGE_NAME; then
             -e ENABLE_ARCHIVELOG=true \
             -e ENABLE_FORCE_LOGGING=true \
             -e ORACLE_SID=$DB_SID \
-            -e ORACLE_USER=$DB_USERNAME \
-            -e ORACLE_PWD=$DB_PASSWORD \
+            -e ORACLE_PWD=$DB_ADMIN_PASSWORD \
             -v $DB_HOST_MOUNT_POINT:/opt/oracle/oradata \
             $IMAGE_NAME
-    fi
 
+        docker exec -i $CONTAINER_NAME sqlplus $DB_ADMIN_USERNAME/$DB_ADMIN_PASSWORD@$DB_HOST:$DB_PORT/$DB_ADMIN_NAME as SYSDBA <<EOF
+            alter user sys identified by $DB_ADMIN_USERNAME;
+            EOF
+        
+        docker exec -i $CONTAINER_NAME sqlplus $ORACLE_USER/$DB_PASSWORD@localhost:$DB_PORT/$DB_NAME as $DB_USERNAME <<EOF
+            CREATE USER $DB_USER_USERNAME IDENTIFIED BY $DB_USER_PASSWORD;
+            GRANT CONNECT, RESOURCE TO $DB_USER_USERNAME;
+            EXIT;
+            EOF
+        
+    fi
 
 else
     #
@@ -62,6 +81,6 @@ else
     #
     echo "Docker image $IMAGE_NAME not found. Please follow the steps in docs/init_database.md to initialize the database."
     echo
-    cat $MY_SCRIPT_DIR/../../docs/init_database.md
+    cat $DOCS/init_database.md
     echo
 fi
