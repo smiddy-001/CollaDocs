aaa4386e97f7357accdd6bcc4ccab93facd5b2df
Riley Smith <Rileys1000@gmail.com>
2024-12-01 23:13:46 +1300

    override API

--- services/colladocs-api/src/test/java/nz/co/colladocs/ColladocsResourceIT.java
+++
@@ -1,8 +0,0 @@
-package nz.co.colladocs;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class ColladocsResourceIT extends ColladocsResourceTest {
-    // Execute the same tests but in packaged mode.
-}
